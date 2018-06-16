766d1ec23662d938e13ae487fde5994459cae482
Jimmy Sánchez <39102908+mscingjimmysanchez@users.noreply.github.com>
2018-06-15 22:06:45 -0500

    Add files via upload

---
+++ Commerce/Commerce/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Commerce.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Application for a webshop.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Contact page.";
+
+            return View();
+        }
+    }
+}
